491d5bfe8c5e0f3e214ff83df5451d41318358b1
fengZhen <fengzhen66@outlook.com>
2018-05-14 16:32:17 +0800

    webpack-demo

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import h1 from './other'
+import css from './index.scss'
+
+let a=2;
+h1.addEventListener('click',()=>{
+    alert('hi')
+})
+alert('sdsf')
---
+++ src/other.js
@@ -0,0 +1,3 @@
+
+let h1 = document.querySelector('h1')
+export default h1
